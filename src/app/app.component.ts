7ba6e5784401cba0530b90d3814891710fae72d8
SamilyLeal <lealsamilysuga@gmail.com>
2024-04-06 17:27:01 -0300

    closes #6 - add cars component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CarComponent } from './components/car/car.component';
+import { CarsComponent } from './components/cars/cars.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CarComponent],
+  imports: [RouterOutlet, HeaderComponent, CarComponent, CarsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/cars/cars.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { Car } from '../../models/Car';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-cars',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './cars.component.html',
+  styleUrl: './cars.component.css'
+})
+export class CarsComponent {
+  cars: Car[] = [
+    {
+      id: 1,
+      model: "corolla",
+      make: "toyota",
+      price: 37000,
+      year: 2007
+    },
+    {
+      id: 2,
+      model: "lancer",
+      make: "mitsubishi",
+      price: 49900,
+      year: 2014
+    }
+  ];
+}
